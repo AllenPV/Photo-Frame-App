c25691d107d399c89cbe5b0133afcae2ba9f2482
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-02-15 21:27:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+    function setup(){
+        Canvas = createCanvas(640,480);
+        Canvas.position(50,150);
+        Video = createCapture(VIDEO);
+        Video.hide();
+}
+function draw(){
+    image(Video,170,150,220,200);
+   
+
+   fill(255,255,0);
+   stroke(255,255,0);
+   rect(40,20,470,50)
+
+   fill(255,0,0);
+   stroke(255,0,0);
+   circle(50,50,80);
+   
+   fill(255,0,0);
+   stroke(255,0,0);
+   circle(520,50,80);
+
+   fill(255,255,0);
+   stroke(255,255,0);
+   rect(40,403,470,50);
+   
+   
+
+   fill(255,0,0);
+   stroke(255,0,0);
+   circle(520,430,80);
+
+   fill(255,0,0);
+   stroke(255,0,0);
+   circle(50,430,80);
+
+   fill(255,255,0);
+   stroke(255,255,0);
+   rect(45,28,50,420);
+
+   fill(255,255,0);
+   stroke(255,255,0);
+   rect(450,28,50,420);
+  
+}
+  
+
+  
+
+   
+
+   
+
+  
+
+   
+
+   
+
+   
+
+
+   
+   
+   
